92ee46dd514b21226de8c6c6842f08ba206b7411
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-04-09 09:58:34 -0600

    update fundamentals

---
+++ code/01-fundamentals/02-multi-views.py
@@ -0,0 +1,41 @@
+from trame.app import get_server
+from trame.widgets import html, client
+from trame.ui.html import DivLayout
+
+# Trame setup -----------------------------------------------------------------
+
+server = get_server()
+
+# View ------------------------------------------------------------------------
+
+# Main view
+with DivLayout(server):  # template_name="main"
+    html.Div("Main View a={{a}} b={{b}}")
+    client.ServerTemplate(name="view_a")
+    client.ServerTemplate(name="view_b")
+
+# view_a
+with DivLayout(server, template_name="view_a"):
+    html.Div("View a={{a}}")
+    html.Input(
+        type="range",
+        min=0,
+        max=100,
+        step=1,
+        v_model=("a", 0),  # Set a default value of 0 to variable "a"
+    )
+
+# view_b
+with DivLayout(server, template_name="view_b"):
+    html.Div("View b={{b}}")
+    html.Input(
+        type="range",
+        min=0,
+        max=50,
+        step=2,
+        v_model=("b", 2),  # Set a default value of 0 to variable "a"
+    )
+
+# Start Server ----------------------------------------------------------------
+
+server.start()
